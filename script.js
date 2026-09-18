420d6b0c2539341efb29d4c2489444ecee1648d9
dangquanganh77 <dangquanganh2277@gmail.com>
2026-09-19 01:09:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 1. Initialize Lenis (Smooth Scroll)
 const lenis = new Lenis({
   duration: 1.2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
   direction: 'vertical',
   gestureDirection: 'vertical',
   smooth: true,
@@ -9,15 +9,9 @@ const lenis = new Lenis({
   touchMultiplier: 2,
 });
 
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
-
 lenis.on('scroll', ScrollTrigger.update);
 gsap.ticker.add((time) => { lenis.raf(time * 1000); });
-gsap.ticker.lagSmoothing(0, 0);
+gsap.ticker.lagSmoothing(0);
 
 // 2. Custom Magnetic Cursor
 const cursor = document.getElementById('custom-cursor');
